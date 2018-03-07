24349bf6619b9cea40ea5d29a7bdf97f55c29855
root <root@LeibowitzPC.localdomain>
2018-03-07 14:33:52 -0600

    ...

--- helloWorld/hello.py
+++ helloWorld/hello.py
@@ -6,9 +6,4 @@ app = Flask(__name__)
 def hello_word():
     return render_template('index.html')
 
-@app.route("/success")
-
-def success():
-    return render_template('success.html')
-
 app.run(debug=True)
