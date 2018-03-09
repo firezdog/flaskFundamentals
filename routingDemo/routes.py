998713d1978ae549712f303f411130149f157c7c
root <root@LeibowitzPC.localdomain>
2018-03-09 14:26:43 -0600

    ...

--- routingDemo/routes.py
+++ routingDemo/routes.py
@@ -1,23 +1,26 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = "none"
 
 @app.route("/")
-
 def index():
     print("Loading index")
     return render_template("index.html")
 
 @app.route("/success")
-
 def success():
     return render_template("success.html")
 
 @app.route("/users", methods=["POST"])
-
 def create_user():
     print "Got Post Info"
-    name = request.form["name"]
-    email = request.form["email"]
-    return redirect("/success")
+    session['name'] = request.form["name"]
+    session['email'] = request.form["email"]
+    return redirect("/show")
+
+@app.route('/show')
+def show_user():
+  return render_template('user.html')
+
 
 app.run(debug=True)
